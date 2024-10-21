e408925f33381865a84c836e015ebffeb980ac69
matteo.angiolillo <matteo.angiolillo@PC0102BIS>
2024-10-21 08:39:24 +0200

    v 0.0.1 aggiunti file base

---
+++ VerificaGit/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VerificaGit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
